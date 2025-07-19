2795b0ed1593759bbe66e280ef2991534d6c8144
Rajas Rakhe <rajas.rakhe@deeptek.ai>
2025-07-19 20:38:07 +0530

    Check Array is sorted and Rotated

---
+++ src/Array/Easy/CheckSortedArray.java
@@ -0,0 +1,33 @@
+package Array.Easy;
+
+class Solution{
+    public boolean check(int[] nums) {
+
+        int count = 0;
+        int n = nums.length;
+
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>nums[i]){
+                count++;
+            }
+        }
+        if(nums[n-1]>nums[0]){
+            count++;
+        }
+        return count<=1;
+
+    }
+}
+
+public class CheckSortedArray {
+
+
+    public static void main(String[] args) {
+
+        int nums[] ={3,4,5,1,2};
+
+        Solution sol = new Solution();
+
+        System.out.println(sol.check(nums));
+    }
+}
